a4de04707ab385f9a6a7ac3bba00d8deb0babbe6
hashimo000 <hashimon.hashimon@icloud.com>
2024-03-11 17:55:59 +0900

    CalendarPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:calendar/Calendar/Calendar.dart';
 void main() {
   runApp((MyApp()),);
 }
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-   
+      home:  CalendarPage(),
     );
   }}
