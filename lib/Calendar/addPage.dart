a77b7c3fa088f517f5f42dd9f2131d3b79797942
hashimo000 <hashimon.hashimon@icloud.com>
2024-03-12 01:44:46 +0900

    戻るボタン

--- lib/Calendar/addPage.dart
+++ lib/Calendar/addPage.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 class AddPage extends ConsumerWidget {
   const AddPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('予定の追加'),
+        backgroundColor: Colors.blueAccent,
+        leading: IconButton(
+          icon: const Icon(Icons.close), // ×アイコンを設定
+          onPressed: () {
+            Navigator.pop(context); // ボタンをタップした時、元の画面に戻る
+          },
+        ),
+      ),
       body: Center(
-        child: Text('Add Pageです。'), ),
+        child: Text('Add Pageです。'),
+      ),
     );
   }
 }
