af01dad111b308e5815055a6084c23cf3106959e
hashimo000 <hashimon.hashimon@icloud.com>
2024-03-12 20:10:56 +0900

    true->yyyy-MM-dd,false->yyyy-MM-dd kk:mm

--- lib/Calendar/addPage.dart
+++ lib/Calendar/addPage.dart
@@ -5,38 +5,77 @@ import 'package:intl/intl.dart';
 
 final dateTimeStartProvider = StateProvider<DateTime>((ref) => DateTime.now());
 final dateTimeEndProvider = StateProvider<DateTime>((ref) => DateTime.now());
-
+final allDayEventProvider = StateProvider<bool>((ref) => false); 
 class AddPage extends ConsumerWidget {
   const AddPage({Key? key,}) : super(key: key);
   
 void _showDateTimePickerStart(BuildContext context, WidgetRef ref) {
-  DatePicker.showDateTimePicker(
-    context,
-    showTitleActions: true,
-    minTime: DateTime(2022, 5, 5, 0, 00),
-    maxTime: DateTime(2030, 6, 7, 23, 59),
-    onConfirm: (date) {
-      ref.read(dateTimeStartProvider.notifier).update((state) => date);
-    },
-    currentTime: ref.watch(dateTimeStartProvider),
-    locale: LocaleType.jp,
-  );
+  final isAllDay = ref.watch(allDayEventProvider);
+  if (isAllDay) {
+    DatePicker.showDatePicker(
+      context,
+      showTitleActions: true,
+      minTime: DateTime(2022, 5, 5),
+      maxTime: DateTime(2030, 6, 7),
+      onConfirm: (date) {
+        // 日付のみを設定
+        ref.read(dateTimeStartProvider.notifier).update((state) => DateTime(date.year, date.month, date.day));
+      },
+      currentTime: ref.watch(dateTimeStartProvider),
+      locale: LocaleType.jp,
+    );
+  } else {
+    // ここでDateTimePickerを表示
+    DatePicker.showDateTimePicker(
+      context,
+      showTitleActions: true,
+      minTime: DateTime(2022, 5, 5, 0, 00),
+      maxTime: DateTime(2030, 6, 7, 23, 59),
+      onConfirm: (date) {
+        ref.read(dateTimeStartProvider.notifier).update((state) => date);
+      },
+      currentTime: ref.watch(dateTimeStartProvider),
+      locale: LocaleType.jp,
+    );
+  }
 }
+
 void _showDateTimePickerEnd(BuildContext context, WidgetRef ref) {
-  DatePicker.showDateTimePicker(
-    context,
-    showTitleActions: true,
-    minTime: DateTime(2022, 5, 5, 0, 00),
-    maxTime: DateTime(2030, 6, 7, 23, 59),
-    onConfirm: (date) {
-      ref.read(dateTimeEndProvider.notifier).update((state) => date);
-    },
-    currentTime: ref.watch(dateTimeEndProvider),
-    locale: LocaleType.jp,
-  );
+  final isAllDay = ref.watch(allDayEventProvider);
+  if (isAllDay) {
+    DatePicker.showDatePicker(
+      context,
+      showTitleActions: true,
+      minTime: DateTime(2022, 5, 5),
+      maxTime: DateTime(2030, 6, 7),
+      onConfirm: (date) {
+        // 日付のみを設定
+        ref.read(dateTimeEndProvider.notifier).update((state) => DateTime(date.year, date.month, date.day));
+      },
+      currentTime: ref.watch(dateTimeEndProvider),
+      locale: LocaleType.jp,
+    );
+  } else {
+    // ここでDateTimePickerを表示
+    DatePicker.showDateTimePicker(
+      context,
+      showTitleActions: true,
+      minTime: DateTime(2022, 5, 5, 0, 00),
+      maxTime: DateTime(2030, 6, 7, 23, 59),
+      onConfirm: (date) {
+        ref.read(dateTimeEndProvider.notifier).update((state) => date);
+      },
+      currentTime: ref.watch(dateTimeEndProvider),
+      locale: LocaleType.jp,
+    );
+  }
 }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isAllDay = ref.watch(allDayEventProvider);
+    final dateFormat = isAllDay ? 'yyyy-MM-dd' : 'yyyy-MM-dd kk:mm';
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('予定の追加'),
@@ -69,12 +108,15 @@ void _showDateTimePickerEnd(BuildContext context, WidgetRef ref) {
                 labelText: ('タイトルを入力してください'),
               ),
             ),
-            SwitchListTile(
-              title: Text('終日'),
-              value: false,
-              onChanged: (bool value) {},
-      secondary: const Icon(Icons.lightbulb_outline),
-            ),
+          SwitchListTile(
+                title: Text('終日'),
+                value: ref.watch(allDayEventProvider), 
+               onChanged: (bool value) {
+               ref.read(allDayEventProvider.notifier).state = value; 
+               },
+               
+               ),
+
             
             GestureDetector(
                   onTap: () => _showDateTimePickerStart(context, ref),
@@ -82,12 +124,7 @@ void _showDateTimePickerEnd(BuildContext context, WidgetRef ref) {
                   builder: (context, ref, child) {
                   final selectedDate = ref.watch(dateTimeStartProvider);
                  return Container(
-        padding: EdgeInsets.all(8.0), // テキスト周りのパディングを8.0に設定
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.blueAccent), // 青色の枠線を設定
-          borderRadius: BorderRadius.circular(4.0), // 角丸の枠にする場合は BorderRadius を設定
-        ),
-                  child:Text("開始"+DateFormat('yyyy-MM-dd kk:mm').format(selectedDate))
+                  child:Text("開始"+DateFormat(dateFormat).format(selectedDate))
                  );
                 },
                ),
@@ -98,7 +135,7 @@ void _showDateTimePickerEnd(BuildContext context, WidgetRef ref) {
                   builder: (context, ref, child) {
                   final selectedDate = ref.watch(dateTimeEndProvider);
                  
-                 return Text("終了"+DateFormat('yyyy-MM-dd kk:mm').format(selectedDate));
+                 return Text("終了"+DateFormat(dateFormat).format(selectedDate));
                 },
                ),
               ),
